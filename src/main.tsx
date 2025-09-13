007a2fcf17fd542c0695a7a8ffa2b717a2a08e6c
RXDCODX <maxxxes73rus@gmail.com>
2025-09-13 14:15:57 +0400

    Add react-metrika dependency and integrate MetrikaCounters in main.tsx for enhanced analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { MetrikaCounters } from 'react-metrika'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App />,
+    <MetrikaCounters ids={[104140647]} options={{ trackHash: true,
+      webvisor: true
+    }} />
   </StrictMode>,
 )
