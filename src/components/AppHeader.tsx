abe142da26a6cfaf792fe8feddd286b58d77f940
RXDCODX <maxxxes73rus@gmail.com>
2026-07-22 14:26:26 +0400

    refactor: удалил раздел с правилами подсчёта очков из хедера
    Интерфейс упростил, перенеся информацию о подсчёте очков в отдельную страницу или раздел. Обновил зависимости, включая React и связанные пакеты.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -23,24 +23,6 @@ const AppHeader: React.FC<AppHeaderProps> = ({ isSplashActive = false }) => {
           />
         </a>
       </div>
-      <div className='logo-delimiter'></div>
-      <div className='points-info'>
-        <div className='points-header'>
-          <h3>Система подсчета очков</h3>
-        </div>
-        <div className='points-grid'>
-          <span>1 место → 11 баллов</span>
-          <span>2 место → 10 баллов</span>
-          <span>3 место → 8 баллов</span>
-          <span>4 место → 7 баллов</span>
-          <span>5-6 место → 6 баллов</span>
-          <span>7-8 место → 5 баллов</span>
-          <span>9-12 место → 4 балла</span>
-          <span>13-16 место → 3 балла</span>
-          <span>17-32 место → 2 балла</span>
-          <span>33+ место → 1 балл</span>
-        </div>
-      </div>
     </header>
   );
 };
